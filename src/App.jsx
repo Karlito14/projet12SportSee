7963ed79158739df966b5d7a6dd24d804aafcee8
Karlito14 <leiroz26@hotmail.com>
2024-05-31 09:17:37 +0200

    ajout note et phrase encoruagement

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ export function App() {
       </header>
       <div className='content'>
         <NavVertical />
-        <main></main>
+        <main className='main'>
+          <h2 className='main__hello'>Bonjour <span className='main__hello__name'>Thomas</span></h2>
+          <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+        </main>
       </div>
     </div>
   );
