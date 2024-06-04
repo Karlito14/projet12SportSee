3394b7eabcd07ce2c9ff2862d08993045dabfee0
Karlito14 <leiroz26@hotmail.com>
2024-06-04 11:44:26 +0200

    affichage titre et légende activité

--- src/components/ChartActivity/ChartActivity.jsx
+++ src/components/ChartActivity/ChartActivity.jsx
@@ -9,32 +9,65 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  Text,
 } from 'recharts';
 
 export const ChartActivity = ({ data }) => {
-  console.log(data);
   return (
-    <ResponsiveContainer className={style.container} height={300} margin= {{top: 20, right: 20, left: 20, bottom:20}}>
+    <ResponsiveContainer className={style.container} height={300}>
+      <div className={style.text}>
+        <h2 className={style.container__title}>Activité quotidienne</h2>
+        <ul className={style.container__list}>
+          <li className={style.container__list__item}>
+            <span className={style.circle_grey}></span>Poids (kg)
+          </li>
+          <li className={style.container__list__item}>
+            <span className={style.circle_red}></span>Calories brûlées (kCal)
+          </li>
+        </ul>
+      </div>
+
       <BarChart
         className={style.container__wrapper}
         width={500}
         height={300}
         data={data.sessions}
         margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
+          top: 15,
+          right: 15,
+          left: 15,
+          bottom: 15,
         }}
         barGap={-30}
+        maxBarSize={10}
       >
-        <CartesianGrid strokeDasharray="1 3" />
-        <XAxis dataKey="day" />
-        <YAxis dataKey="kilogram" tickMargin={30} domain={[0, 'auto']}/>
+        <CartesianGrid strokeDasharray="1 2" />
+        <XAxis
+          dataKey="day"
+
+          //scale={'point'}
+        />
+        <YAxis
+          dataKey="kilogram"
+          tickMargin={30}
+          domain={['dataMin - 3', 'dataMax + 3']}
+          orientation="right"
+          tickSize="0"
+        />
         <Tooltip />
-        <Legend verticalAlign="top" height={36} align='right' iconType='circle' />
-        <Bar dataKey="kilogram" fill="#282D30" activeBar={<Rectangle />} radius={[20, 20, 0, 0]} maxBarSize={10} />
-        <Bar dataKey="calories" fill="#E60000" activeBar={<Rectangle />} radius={[20, 20, 0, 0]} maxBarSize={10} />
+        {/*<Legend verticalAlign="top" align="right" iconType="circle" />*/}
+        <Bar
+          dataKey="kilogram"
+          fill="#282D30"
+          activeBar={<Rectangle />}
+          radius={[20, 20, 0, 0]}
+        />
+        <Bar
+          dataKey="calories"
+          fill="#E60000"
+          activeBar={<Rectangle />}
+          radius={[20, 20, 0, 0]}
+        />
       </BarChart>
     </ResponsiveContainer>
   );
