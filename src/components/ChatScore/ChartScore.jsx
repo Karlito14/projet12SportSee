85cc357a3ecb1a6ce390913f6348d038d3f19b6f
Karlito14 <leiroz26@hotmail.com>
2024-06-05 11:40:18 +0200

    css emplacement graphiques

---
+++ src/components/ChatScore/ChartScore.jsx
@@ -0,0 +1,31 @@
+import style from './style.module.scss';
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  ResponsiveContainer,
+} from 'recharts';
+
+export const ChartScore = ({ data }) => {
+  console.log(data);
+  return (
+    <div className={style.container}>
+      <ResponsiveContainer width="100%" height="200px">
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis />
+          <Radar
+            name="Mike"
+            dataKey="A"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.6}
+          />
+        </RadarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,28 +5,37 @@ import style from './style.module.scss';
 import { NutritionalList } from '../../components/NutritionalList/NutritionalList.jsx';
 import { ChartActivity } from '../../components/ChartActivity/ChartActivity.jsx';
 import { ChartSessions } from '../../components/ChartSessions/ChartSessions.jsx';
+import { ChartPerformance } from '../../components/ChartPerformance/ChartPerformance.jsx';
+import { ChartScore } from '../../components/ChatScore/ChartScore.jsx';
 
 export const Dashboard = () => {
   const [user, setUser] = useState();
   const [activity, setActivity] = useState();
   const [sessions, setSessions] = useState();
+  const [performance, setPerformance] = useState();
+
   const { id } = useParams();
 
-  const getUser = async () => {
+  const getData = async () => {
     try {
       const user = await ApiUser.fetchUser(id);
       setUser(user);
+
       const activity = await ApiUser.getActivity(id);
       setActivity(activity);
+
       const sessions = await ApiUser.getSessions(id);
       setSessions(sessions);
+
+      const performance = await ApiUser.getPerformance(id);
+      setPerformance(performance);
     } catch (error) {
       console.error(error.message);
     }
   };
 
   useEffect(() => {
-    getUser();
+    getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -44,6 +53,8 @@ export const Dashboard = () => {
           {activity && <ChartActivity data={activity} />}
           <div className={style.content__recharts__bottom}>
             {sessions && <ChartSessions data={sessions} />}
+            {performance && <ChartPerformance data={performance} />}
+            <ChartScore />
           </div>
         </div>
         {user && <NutritionalList list={user.keyData} />}
