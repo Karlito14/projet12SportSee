33c8d8100e6b4f131db33a24c748832ab9392f57
Karlito14 <leiroz26@hotmail.com>
2024-06-24 10:33:59 +0200

    initialisation du swagger

--- src/components/ErrorDisplay/ErrorDisplay.jsx
+++ src/components/ErrorDisplay/ErrorDisplay.jsx
@@ -3,7 +3,9 @@ import style from './style.module.scss';
 export const ErrorDisplay = ({ height, width }) => {
   return (
     <div className={style.container} style={{ height: height, width: width }}>
-        <p className={style.container__error}>Impossible d&apos;afficher le composant pour le moment</p>
+      <p className={style.container__error}>
+        Impossible d&apos;afficher le composant pour le moment
+      </p>
     </div>
-  )
+  );
 };
