93ec1d7bb0a099ece767725c459eaac519094315
Karlito14 <leiroz26@hotmail.com>
2024-06-25 11:31:25 +0200

    export des données au format xlsx

--- src/components/ExportCSV/ExportCSV.jsx
+++ src/components/ExportCSV/ExportCSV.jsx
@@ -1,18 +1,35 @@
 import style from './style.module.scss';
-import { CSVLink } from 'react-csv';
+// import { CSVLink } from 'react-csv';
+import { utils, writeFile} from 'xlsx';
+
+// export const ExportCSV = ({ data }) => {
+//   console.log(data);
+//   const headers = [
+//     { label: 'Jour', key: 'day' },
+//     { label: 'Calories dépensées', key: 'calories' },
+//     { label: 'Poids (kg)', key: 'kilogram' },
+//     { label: 'Session (min)', key: 'sessionLength' },
+//   ];
+
+//   return (
+//     <CSVLink data={data} headers={headers} filename='données' target='_blank' className={style.button}>
+//       Exporter
+//     </CSVLink>
+//   );
+// };
 
 export const ExportCSV = ({ data }) => {
-  console.log(data);
-  const headers = [
-    { label: 'Jour', key: 'day' },
-    { label: 'Calories dépensées', key: 'calories' },
-    { label: 'Poids (kg)', key: 'kilogram' },
-    { label: 'Session (min)', key: 'sessionLength' },
-  ];
+  const handleExport = () => {
+    const workbook = utils.book_new();
+    const worksheet = utils.json_to_sheet(data)
+
+    utils.book_append_sheet(workbook, worksheet, 'données')
+    writeFile(workbook, 'données.xlsx')
+  };
 
   return (
-    <CSVLink data={data} headers={headers} filename='données' target='_blank' className={style.button}>
+    <button onClick={handleExport} className={style.button}>
       Exporter
-    </CSVLink>
+    </button>
   );
 };
